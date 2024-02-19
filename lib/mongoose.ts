b1ae6361a17f2fef246d5892afe5b9dedbd85fa9
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-02-19 10:52:48 +0530

    Update mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -5,12 +5,12 @@ let isConnected = false;// Variable to track the connection status
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
+  if(!"mongodb+srv://Sherma:Sherma@sst.ps6ceqg.mongodb.net/?retryWrites=true&w=majority") return console.log('MONGODB_URI is not defined');
 
   if(isConnected) return console.log('=> using existing database connection');
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect("mongodb+srv://Sherma:Sherma@sst.ps6ceqg.mongodb.net/?retryWrites=true&w=majority");
 
     isConnected = true;
 
