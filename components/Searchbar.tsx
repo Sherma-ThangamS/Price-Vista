e8ca5ae54d7bc2d149582a42c0ee27996a8c121e
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-02-19 13:27:13 +0530

    Update Searchbar.tsx

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { scrapeAndStoreProduct } from '@/lib/actions';
+import { useRouter } from 'next/navigation';
 import { FormEvent, useState } from 'react'
 
 const isValidAmazonProductURL = (url: string) => {
@@ -22,37 +23,42 @@ const isValidAmazonProductURL = (url: string) => {
   return false;
 }
 
+
 const Searchbar = () => {
   const [searchPrompt, setSearchPrompt] = useState('');
   const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter(); // Move the hook call inside the component body
 
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-    if(!isValidLink) return alert('Please provide a valid Amazon link')
+    if (!isValidLink) return alert('Please provide a valid Amazon link');
 
     try {
       setIsLoading(true);
 
       // Scrape the product page
       const product = await scrapeAndStoreProduct(searchPrompt);
-      setSearchPrompt("")
-      console.log(product)
+      setSearchPrompt("");
+      console.log(product);
+
+      router.push(`/products/${product}`);
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  }
+  };
 
   return (
-    <form 
-      className="flex flex-wrap gap-4 mt-12" 
+    <form
+      className="flex flex-wrap gap-4 mt-12"
       onSubmit={handleSubmit}
     >
-      <input 
+      <input
         type="text"
         value={searchPrompt}
         onChange={(e) => setSearchPrompt(e.target.value)}
@@ -60,15 +66,15 @@ const Searchbar = () => {
         className="searchbar-input"
       />
 
-      <button 
-        type="submit" 
+      <button
+        type="submit"
         className="searchbar-btn"
         disabled={searchPrompt === ''}
       >
         {isLoading ? 'Searching...' : 'Search'}
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default Searchbar
+export default Searchbar;
