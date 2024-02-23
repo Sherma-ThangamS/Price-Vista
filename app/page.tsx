62679967547627ac51966387a6fe811d0b62050f
Sherma Thangam S <119473142+Sherma-ThangamS@users.noreply.github.com>
2024-02-23 17:37:11 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ const Home = async () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16 ">
-          allProducts?.map((product) => (
+          {allProducts?.map((product) => (
             product.image?<ProductCard key={product._id as string} product={product as Product} />:""
           ))}
         </div>
