894c3ab9d838abbe281975abab30a20ee73d7de9
PIAI23MAR1010 <sshermathangam@gmail.com>
2024-12-25 18:35:08 +0530

    Email Update

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -95,7 +95,7 @@ const transporter = nodemailer.createTransport({
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
   const mailOptions = {
-    from: 'bug321123bug@gmail.com',
+    from: 'shermathangam.s@outlook.com',
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
